bc3030aa6e92e03284788c1c85c12dda4157aaf4
mdrifat59 <mdrifatulislam59@gmail.com>
2025-07-26 11:43:06 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react' 
+import NavBar from './component/navbar.component'
+import Search from './component/search.component'
+ 
+
+function App() { 
+
+  return (
+    <>
+      <NavBar/>
+      <Search/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/navbar.component.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+ class Navbar extends Component {
+  render() {
+    return (
+      <div>navbar.component</div>
+    )
+  }
+}
+export default Navbar
